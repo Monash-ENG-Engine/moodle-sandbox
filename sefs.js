acdb076c4ba0f34a17679b6251448a343d34c1ef
elizabethmccarthy-maker <elizabeth.mccarthy@monash.edu>
2026-08-17 07:29:57 +1000

    Update sefs.js

--- sefs.js
+++ sefs.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 
     let sefHTML = `
         <div class='my-3 eng-sefs'>
-            <a href='https://monash.syd1.qualtrics.com/jfe/form/SV_aaTRJEHrhHBGvEq' title='Engineering Student Experience Feedback Survey (SEFS)'>
+            <a href='https://monash.az1.qualtrics.com/jfe/form/SV_aaTRJEHrhHBGvEq' title='Engineering Student Experience Feedback Survey (SEFS)'>
                 <img src='https://monash-eng-engine.github.io/moodle-sandbox/SEFS-S2-2026-Header.jpg' alt='Student Experience Feedback Survey (SEFS). The mid-semester feedback forum for your engineering units. Closes 29 March.' width="100%"/>
                 <img src='https://monash-eng-engine.github.io/moodle-sandbox/SEFS-S2-2026-Footer.jpg' alt='Click here to complete the Engineering Student Experience Feedback Survey (SEFS) for a chance to win one of 5 100 dollar vourchers' width="100%"/>
             </a>
